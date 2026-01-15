e7d93eefb58428a5d29b2e214e40de6643429b7a
Angel Carballeira <angelcarballeira@gmail.com>
2026-01-15 09:04:12 -0300

    fix: redirect root route to login

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/Login';
 import Dashboard from '../pages/Dashboard';
 import Admin from '../pages/Admin';
@@ -8,6 +8,8 @@ import ProtectedRoute from '../auth/ProtectedRoute';
 const AppRouter = () => (
   <BrowserRouter>
     <Routes>
+      <Route path='/' element={<Navigate to='/login' replace />} />
+
       <Route path='/login' element={<Login />} />
 
       <Route
