59e2b1c47a8c9962d89247f9da711212144781a6
Angel Carballeira <angelcarballeira@gmail.com>
2026-01-15 10:15:14 -0300

    feat: fake auth api with login simulation

---
+++ src/api/authApi.js
@@ -0,0 +1,18 @@
+import { users } from '../data/users';
+
+export const loginRequest = (username, password) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const user = users.find(
+        (u) => u.username === username && u.password === password
+      );
+
+      if (!user) {
+        reject('Invalid credentials');
+      }
+
+      const { password: _, ...safeUser } = user;
+      resolve(safeUser);
+    }, 800);
+  });
+};
